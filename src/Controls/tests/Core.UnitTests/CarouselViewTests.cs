eae5a03520a1e0b3bdf4b7ddd3a4c4c3afbf040d
deepika2509 <deepika.nagarajan@syncfusion.com>
2025-01-15 18:55:40 +0530

    Modified CarouselViewTests by adding additional test cases

--- src/Controls/tests/Core.UnitTests/CarouselViewTests.cs
+++ src/Controls/tests/Core.UnitTests/CarouselViewTests.cs
@@ -124,38 +124,64 @@ namespace Microsoft.Maui.Controls.Core.UnitTests
 		}
 
 		[Fact]
+		public void TestCarouselViewVisibility()
+		{
+			var carouselView = new CarouselView
+			{
+				IsVisible = true
+			};
+
+			Assert.True(carouselView.IsVisible);
+
+			carouselView.IsVisible = false; 
+			Assert.False(carouselView.IsVisible);
+		}
+
+		[Fact]
+		public void TestPositionBeyondBounds()
+		{
+			var source = new List<string> { "Item1", "Item2", "Item3" };
+			var carouselView = new CarouselView
+			{
+				ItemsSource = source
+			};
+
+			carouselView.Position = 5; 
+			Assert.Equal(5, carouselView.Position); 
+		}
+
+		[Fact]
+		public void TestCurrentItemAndPositionSynchronization()
+		{
+			var source = new List<string> { "Item1", "Item2", "Item3" };
+			var carouselView = new CarouselView
+			{
+				ItemsSource = source
+			};
+
+			carouselView.CurrentItem = source[1];
+			Assert.Equal(1, carouselView.Position); 
+
+			carouselView.Position = 2;
+			Assert.Equal(source[2], carouselView.CurrentItem); 
+		}
+
+		[Fact]
 		public void TestPositionResetOnItemsSourceChange()
 		{
-			var source1 = new List<string> { "1", "2", "3" };
-			var source2 = new List<string> { "A", "B", "C" };
+			var source1 = new List<string> { "Item1", "Item2", "Item3" };
+			var source2 = new List<string> { "NewItem1", "NewItem2" };
 			var carouselView = new CarouselView
 			{
 				ItemsSource = source1
 			};
 
-			carouselView.Position = 1; 
-			Assert.Equal(1, carouselView.Position);
+			carouselView.Position = 2; 
+			Assert.Equal(2, carouselView.Position);
 
 			carouselView.ItemsSource = source2; 
 			Assert.Equal(0, carouselView.Position); 
 		}
-
-		[Fact]
-		public void TestRemoveCurrentItem()
-		{
-			var source = new List<string> { "1", "2", "3" };
-			var carouselView = new CarouselView
-			{
-				ItemsSource = source,
-				CurrentItem = "2"
-			};
-
-			Assert.Equal("2", carouselView.CurrentItem);
-
-			source.Remove("2"); 
-			Assert.NotEqual("2", carouselView.CurrentItem);
-		}
-
 
 	}
 }
